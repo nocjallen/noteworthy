80e73949b874fa1a7f42a133d0e65543caa90a7c
Jonathan Allen <nocjallen@protonmail.com>
2019-02-08 00:51:14 -0500

    Created buttons for note selection and CSS/JS to display the actual note and whether or not it was correct.

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,32 @@
 window.onload = function() {
+    var answers = document.getElementById("answers");
+    var btnGuess = document.querySelectorAll(".btn-guess");
     var btnMoveNoteUp = document.getElementById("move-note-up");
     var btnMoveNoteDown = document.getElementById("move-note-down");
-    
-    var noteArray = ["note-a", "note-b", "note-c", "note-d", "note-e", "note-f", "note-g"];
-    var i = 3
+    var div = document.createElement("div");
 
-    document.getElementById("note").classList.toggle(noteArray[i]);
+    var notePositions = ["note-a", "note-b", "note-c", "note-d", "note-e", "note-f", "note-g"];
+    var notesBass = ["A", "B", "C", "D", "E", "F", "G"];
+    var randomNote = Math.floor(Math.random() * (notePositions.length -1));
+
+    document.getElementById("note").classList.toggle(notePositions[randomNote]);
+
+    for (var i = 0; i < btnGuess.length; i++) {
+        btnGuess[i].addEventListener('click', function() {
+            div.innerHTML = (this.textContent);
+            div.className = "answer-box";
+            div.style.marginLeft = "95px";
+            if (this.textContent == notesBass[randomNote]) {
+                div.style.backgroundColor = "green";
+            } else {
+                div.style.backgroundColor = "red";
+            };
+            answers.appendChild(div);
+        });
+    }
+
+    
+
 
 
     
@@ -19,24 +40,24 @@ window.onload = function() {
 
 
     function moveNoteUp() {
-        if (i != 6) {
-            var currentNote = i;
-            i ++;
-            var nextNote = i;
+        if (randomNote != 6) {
+            var currentNote = randomNote;
+            randomNote ++;
+            var nextNote = randomNote;
 
-            document.getElementById("note").classList.toggle(noteArray[currentNote]);
-            document.getElementById("note").classList.toggle(noteArray[nextNote]);
+            document.getElementById("note").classList.toggle(notePositions[currentNote]);
+            document.getElementById("note").classList.toggle(notePositions[nextNote]);
         }
     }
 
     function moveNoteDown() {
-        if (i != 0) {
-            var currentNote = i;
-            i --;
-            var nextNote = i;
+        if (randomNote != 0) {
+            var currentNote = randomNote;
+            randomNote --;
+            var nextNote = randomNote;
 
-            document.getElementById("note").classList.toggle(noteArray[currentNote]);
-            document.getElementById("note").classList.toggle(noteArray[nextNote]);
+            document.getElementById("note").classList.toggle(notePositions[currentNote]);
+            document.getElementById("note").classList.toggle(notePositions[nextNote]);
         }
     }
     
