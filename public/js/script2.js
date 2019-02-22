25aa4ce9b15f70f750a6588c9b5ca1929f21d981
Jonathan Allen <nocjallen@protonmail.com>
2019-02-21 20:09:43 -0500

    Added dev note and rewrote JS functions to move the dev note on the staff. Adjusted bar CSS in to provide space for notes at the top and bottom of the staff. Bar one is now fully functional.

--- public/js/script2.js
+++ public/js/script2.js
@@ -1,30 +1,37 @@
-var answerMargin = 80;
-var answersDiv = document.getElementById("answers");
+var answerMargin = 20;
+var answersOneDiv = document.getElementById("answers-one");
 var btnGuess = document.querySelectorAll(".btn-guess");
 var btnMoveNoteUp = document.getElementById("move-note-up");
 var btnMoveNoteDown = document.getElementById("move-note-down");
 var cleff = "bass";
 var currentNote = 0;
+var devNote = document.getElementById("dev-note");
+var devNotePos = 3;
 var notes = [];
-var notesBass = [["pos-1", "A"],["pos-2", "B"],["pos-3", "C"],["pos-4", "D"],["pos-5", "E"],["pos-6", "F"],["pos-7", "G"]];
+const notesBass = [['pos-1', 'A'],['pos-2', 'B'],['pos-3', 'C'],['pos-4', 'D'],['pos-5', 'E'],['pos-6', 'F'],['pos-7', 'G']];
+var barOneDiv = document.getElementById("bar-one");
+var barTwoDiv = document.getElementById("bar-two");
 var notesDiv = document.getElementById("notes");
+
+devNote.classList.toggle(notesBass[devNotePos][0]);
 
 createNotes(4);
 
 function createNotes(numNotes) {
-    var margin = 80;
-    var noteChoices = notesBass;
-    //console.log("noteChoices.length " + noteChoices.length);
+    var margin = 20;
+    var noteChoices = [];
+    for (var i = 0; i < notesBass.length; i++) {
+        noteChoices.push(notesBass[i]);
+    }
     for (var i = 0; i < numNotes; i++) {
         var div = document.createElement("div");
         var randomNote = Math.floor(Math.random() * (noteChoices.length -1));
-        //console.log("randomNote " + randomNote);
         if (cleff == "bass") {
             notes.push(noteChoices[randomNote][1]);
         }
         div.innerHTML += '<img class="note ' + noteChoices[randomNote][0] + ' " src="public/img/note_quarter.svg" alt="Quarter Note"></img>';
         div.style.marginLeft = margin + "px";
-        notesDiv.appendChild(div);
+        barOneDiv.appendChild(div);
         margin += 40;
         noteChoices.splice(randomNote, 1);
     }
@@ -44,14 +51,12 @@ for (var i = 0; i < btnGuess.length; i++) {
             } else {
                 div.style.backgroundColor = "red";
             };
-            answersDiv.appendChild(div);
+            answersOneDiv.appendChild(div);
             answerMargin += 40;
             currentNote += 1;
         }
     });
 }
-
-
 
 btnMoveNoteUp.addEventListener("click", function() {
     moveNoteUp();
@@ -63,23 +68,17 @@ btnMoveNoteDown.addEventListener("click", function() {
 
 
 function moveNoteUp() {
-    if (randomNote != 6) {
-        var currentNote = randomNote;
-        randomNote ++;
-        var nextNote = randomNote;
-
-        document.getElementById("note").classList.toggle(notePositions[currentNote]);
-        document.getElementById("note").classList.toggle(notePositions[nextNote]);
+    if (devNotePos != 6) {
+        devNote.classList.toggle(notesBass[devNotePos][0]);
+        devNotePos ++;
+        devNote.classList.toggle(notesBass[devNotePos][0]);
     }
 }
 
 function moveNoteDown() {
-    if (randomNote != 0) {
-        var currentNote = randomNote;
-        randomNote --;
-        var nextNote = randomNote;
-
-        document.getElementById("note").classList.toggle(notePositions[currentNote]);
-        document.getElementById("note").classList.toggle(notePositions[nextNote]);
+    if (devNotePos != 0) {
+        devNote.classList.toggle(notesBass[devNotePos][0]);
+        devNotePos --;
+        devNote.classList.toggle(notesBass[devNotePos][0]);
     }
 }
